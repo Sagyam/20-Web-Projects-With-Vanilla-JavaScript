04893dc5010085543e6fa74138f9af720efdcea0
Sagyam Thapa <sagyamthapa32@gmail.com>
2021-04-27 08:46:09 +0545

    modal-menu-slider complete

---
+++ modal-menu-slider/script.js
@@ -0,0 +1,21 @@
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+//Toggle nav
+toggle.addEventListener("click", () =>
+	document.body.classList.toggle("show-nav")
+);
+
+//Show Modal
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+//Hide Modal
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+//Hide moal on outside click
+
+window.addEventListener("click", (e) =>
+	e.target == modal ? modal.classList.remove("show-modal") : false
+);
